3db52c515d0cdc3dd878727e551490f0fc15bd8e
xxraatata <aratsaniyah97@gmail.com>
2025-07-10 14:12:57 +0700

    ta

--- src/main/java/id/co/dbravelo/repository/ReviewFotoRepo.java
+++ src/main/java/id/co/dbravelo/repository/ReviewFotoRepo.java
@@ -4,5 +4,8 @@ import id.co.dbravelo.model.ReviewFoto;
 import id.co.dbravelo.model.ReviewMakanan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewFotoRepo extends JpaRepository<ReviewFoto, Integer> {
+    List<ReviewFoto> findByReviewId(int reviewId);
 }
